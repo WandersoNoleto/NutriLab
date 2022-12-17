2b2372643020fe32264d8b2f0ffced0cc6baa2ff
WandersoNoleto <onlywanderson@gmail.com>
2022-12-17 15:45:55 -0300

    auth registers & messages config

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,10 @@
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .utils import password_is_valid
 
 
 def register(request):
@@ -11,9 +16,22 @@ def register(request):
          password = request.POST.get('senha')
          confirm_password = request.POST.get('confirmar_senha')
          email    = request.POST.get('email')
-         
 
-         return HttpResponse(confirm_password)
+         if not password_is_valid(request, password, confirm_password):
+            return redirect('/auth/cadastro')
+         
+         try:
+            user = User.objects.create_user(username=username,
+																		email=email,
+                                    password=password,
+                                    is_active=False)
+            user.save()
+            messages.add_message(request, constants.ERROR, 'Usuario cadastrado com sucesso')
+            return redirect('/auth/login')
+         
+         except Exception as error:
+            messages.add_message(request, constants.ERROR, '[ERROR]:' + error)
+            return redirect('/auth/cadastro')
 
 def login(request):
     return HttpResponse("Voce está na pagina de login")
--- nutri_lab/settings.py
+++ nutri_lab/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+from django.contrib.messages import constants
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -136,3 +137,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
