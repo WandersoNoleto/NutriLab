92744766460ff91e4d9430b183db977d0bc39938
WandersoNoleto <onlywanderson@gmail.com>
2022-12-19 18:33:43 -0300

    switch name project

--- nutrily_app/asgi.py
+++ nutrily_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for nutri_lab project.
+ASGI config for nutrily_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutri_lab.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutrily_app.settings')
 
 application = get_asgi_application()
--- nutrily_app/wsgi.py
+++ nutrily_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nutri_lab project.
+WSGI config for nutrily_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutri_lab.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutrily_app.settings')
 
 application = get_wsgi_application()
